9d2988801a3d506c75ab3b705e32e67b2311037b
ARIAMJ Group <Ariamjgroup@gmail.com>
2026-06-29 13:42:51 +0330

    Fix Navigation component

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,13 +1,5 @@
 import Link from "next/link";
 import { navigationItems } from "./header-data";
-  { name: "Home", href: "/" },
-  { name: "About", href: "/about" },
-  { name: "Services", href: "/services" },
-  { name: "Industries", href: "/industries" },
-  { name: "Projects", href: "/projects" },
-  { name: "Blog", href: "/blog" },
-  { name: "Contact", href: "/contact" },
-];
 
 export default function Navigation() {
   return (
